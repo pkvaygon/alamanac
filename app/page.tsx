ca72104cf11708bcae1cb4e2b82ed9ba61cdb2e2
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-03-03 13:26:57 -0600

    link to new alamanac

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { ImageDataProps } from "./new-almanac/page";
+import Link from "next/link";
 
  async function getAllPosts() {
   const res = await fetch('https://us-central1.gcp.data.mongodb-api.com/app/almanac-nwvhl/endpoint/get_post');
@@ -10,6 +11,8 @@ export default async function Home() {
   const posts = await getAllPosts();
 console.log(posts[0])
   return (
+    <>
+      <Link href="new-almanac">Создать пост</Link>
     <section className="flex flex-col gap-3">
       <div className="relative w-full h-[400px]">
       <Image fill priority src={posts[0].postImage.url} objectFit="contain" alt={posts[0].postTitle} />
@@ -29,6 +32,7 @@ console.log(posts[0])
           ))}
         </div>
       ))}
-    </section>
+      </section>
+      </>
   );
 }
